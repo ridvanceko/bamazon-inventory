444f9eed12ff29f19282621dbd272e5f1fd67af5
Ridvan Cekmez <ridvanceko@gmail.com>
2019-10-22 21:41:22 -0700

    I created my table and list

---
+++ bamazon.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+let connection = mysql.createConnection({
+  host:"localhost",
+  port:3306,
+  user:"root",
+  password:"root",
+  database:"bamazon"
+})
+
+connection.connect(function(err){
+  if(err) throw err;
+  console.log("Connected");
+})
+
+
+

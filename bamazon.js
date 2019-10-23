2d748e39e5029451916871956e1d62e269bf96bc
Ridvan Cekmez <ridvanceko@gmail.com>
2019-10-23 13:34:43 -0700

    Mysql database printed at the screen

--- bamazon.js
+++ bamazon.js
@@ -12,7 +12,17 @@ let connection = mysql.createConnection({
 connection.connect(function(err){
   if(err) throw err;
   console.log("Connected");
+  makeTable();
 })
+
+var makeTable = function(){
+  connection.query("SELECT * FROM products", function(err,res){
+    for(var i=0; i<res.length; i++){
+      console.log(res[i].itemid+" - "+res[i].product_name+" - "+
+      res[i].department_name+" - "+res[i].price+" - "+res[i].stock_quantity+"\n");
+    }
+  })
+}
 
 
 
